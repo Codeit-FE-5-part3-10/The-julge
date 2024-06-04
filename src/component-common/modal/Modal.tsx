80231791b5bc2622392621664f3e4205a89e6b81
GoldenHamsterK <kimdajin0928@gmail.com>
2024-06-05 00:37:08 +0900

    fix: 모달창(기본) 수정 및 스타일 적용

--- src/component-common/modal/Modal.tsx
+++ src/component-common/modal/Modal.tsx
@@ -1,57 +1,83 @@
 /*
 사용방법
-<Modal
-  setModal={모달 setState}
-  modalItems={
-    아래 세개 중 해당되는 사항으로 기입해주세요.
-    warning = 느낌표 아이콘 && 버튼 한개 
-    success = 아이콘X && 버튼 한개 
-    question = 체크 아이콘 && 버튼 두개
-  }
-  text={모달 안내문구}
-  link={클릭시 이동 할 링크}
-  btnText={버튼 두개인 경우 메인 컬러 버튼문구}
-/>
-*/
 
-/*
-Todo
--디버깅
--스타일(순서, classname styles? 확인필요 )
--버튼 컴포넌트? 스타일 넣으면 반영
-*/
+modalType 아래 세개 중 해당되는 사항으로 기입해주세요.
+warning = 느낌표 아이콘 && 버튼 한개 
+success = 아이콘X && 버튼 한개 
+question = 체크 아이콘 && 버튼 두개
 
-import "@styles/global.scss";
-import styles from "./Modal.module.scss";
+예시) 
 
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import questionIcon from "public/images/modal-question.svg";
-import warningIcon from "public/images/modal-alert.svg";
-
-interface ModalProps {
-  setModal: (value: boolean) => void;
-  modalItems: {
-    content: string;
-    modalType: "warning" | "success" | "question";
-  };
-  text?: string;
+interface ModalItems {
+  content: string;
+  modalType: "warning" | "success" | "question";
   link: string;
   btnText: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ setModal, modalItems, text, link, btnText = "예" }) => {
+  const [isModalOpen, setModalOpen] = useState<ModalItems | null>(null);
 
+  const showModal = () => {
+    setModalOpen({
+      content: "모달 안내문구",
+      modalType: "success",
+      link: "https://naver.com"
+      btnText: "question에서 메인컬러 버튼 Text",
+    });
+  };
+
+  <button onClick={showModal} className="ToolBtn">
+    버튼
+  </button>
+  {isModalOpen && (
+    <Modal
+      setModal={setModalOpen}
+      modalItems={isModalOpen}
+      content={isModalOpen.content}
+      link={isModalOpen.link}
+      btnText={isModalOpen.btnText}
+    />
+  )}
+
+*/
+
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import classNames from "classnames/bind";
+import styles from "./Modal.module.scss";
+import questionIcon from "public/images/modal-question.svg";
+import warningIcon from "public/images/modal-alert.svg";
+
+const cx = classNames.bind(styles);
+interface ModalProps {
+  setModal: (value: ModalItems | null) => void;
+  modalItems: {
+    content: string;
+    modalType: "warning" | "success" | "question";
+  };
+  content?: string;
+  link: string;
+  btnText: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  setModal,
+  modalItems,
+  content,
+  link,
+  btnText = "예",
+}) => {
   const closeModal = () => {
-    setModal(false);
+    setModal(null);
   };
 
   const getModalImg = () => {
     switch (modalItems.modalType) {
       case "success":
         return null;
-      case "warning":
+      case "warning": 
         return warningIcon;
       case "question":
         return questionIcon;
@@ -65,23 +91,23 @@ const Modal: React.FC<ModalProps> = ({ setModal, modalItems, text, link, btnText
       case "success":
         return (
           <Link href={link} passHref>
-            <button className={styles.mainBtn}>확인</button>
+            <button className={cx("mainBtn")}>확인</button>
           </Link>
         );
       case "warning":
         return (
-          <button className={styles.modalClose} onClick={closeModal}>
+          <button className={cx("modalClose")} onClick={closeModal}>
             확인
           </button>
         );
       case "question":
         return (
           <>
-            <button className={styles.modalClose} onClick={closeModal}>
+            <button className={cx("modalClose")} onClick={closeModal}>
               아니요
             </button>
             <Link href={link} passHref>
-              <button className={styles.mainBtn}>{btnText}</button>
+              <button className={cx("mainBtn")}>{btnText}</button>
             </Link>
           </>
         );
@@ -91,11 +117,13 @@ const Modal: React.FC<ModalProps> = ({ setModal, modalItems, text, link, btnText
   };
 
   return (
-    <section className={styles.modalBack} onClick={closeModal}>
-      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-        {getModalImg() && <Image src={getModalImg() || ""} alt="modalIcon" />}
-        <div className={styles.modaltext}>{text}</div>
-        {getModalBtn()}
+    <section className={cx("modalBack")} onClick={closeModal}>
+      <div className={cx("modal")} onClick={(e) => e.stopPropagation()}>
+        <div className={cx("modalwrap")}>
+          {getModalImg() && <Image src={getModalImg() || ""} alt="modalIcon" />}
+          <div className={cx("modaltext")}>{content}</div>
+          {getModalBtn()}
+        </div>
       </div>
     </section>
   );
