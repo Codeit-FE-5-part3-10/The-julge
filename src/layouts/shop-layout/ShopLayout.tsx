e0cd5ae41be8eb883c8321a3acef551aa421c843
BaeUiJin <baeuijin01@gmail.com>
2024-06-05 12:57:06 +0900

    refactor: 디렉토리 구조 변경

---
+++ src/layouts/shop-layout/ShopLayout.tsx
@@ -0,0 +1,9 @@
+import styles from "./";
+import classNames from "classnames/bind";
+import React from "react";
+
+const cx = classNames.bind(styles);
+
+export const ShopLayout = () => {
+  return <div></div>;
+};
