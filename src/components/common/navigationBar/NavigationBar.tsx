0ce11518c7fbc223f654b010a3432071aa38e202
wjy308 <65326956+wjy308@users.noreply.github.com>
2024-06-05 17:26:23 +0900

    feat:네비게이션 바 레이아웃 구현

--- src/components/common/navigationBar/NavigationBar.tsx
+++ src/components/common/navigationBar/NavigationBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import classNames from "classnames/bind";
 import styles from "./NavigationBar.module.scss";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchBarIcon from "@/public/images/navigationbar-search.svg";
 import NotificationIcon from "@/public/images/navigationbar-empty.svg";
 import NotificationIconNew from "@/public/images/navigationbar-new.svg";
@@ -11,19 +11,68 @@ import NotificationIconNew from "@/public/images/navigationbar-new.svg";
 export default function NavigationBar({}) {
   const cx = classNames.bind(styles);
   const [searchTerm, setSearchTerm] = useState("");
-  const isLoggedIn = 0;
+  const [isTablet, setIsTablet] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTablet(window.innerWidth >= 768); // 예시로 768px 이상을 테블릿으로 설정
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // 초기 로드 시 크기 체크
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className={cx("navBar")}>
       <div className={cx("logoButton_container")}>
         <div className={cx("logo_container")}>
           <Link href="/" className={cx("logo")}>
-            <Image src={Logo} alt={""}></Image>
+            <Image src={Logo} alt="로고"></Image>
           </Link>
         </div>
 
-        {/* Search */}
-        <div className={cx("searchBar-container")}>
+        {isTablet && (
+          <div className={cx("searchBar-container-tablet")}>
+            <input
+              className={cx("searchBar")}
+              type="text"
+              placeholder="가게 이름으로 찾아보세요"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Image
+              className={cx("searchBarIcon")}
+              src={SearchBarIcon}
+              alt="검색 아이콘"
+            />
+          </div>
+        )}
+
+        <div className={cx("buttons-container")}>
+          {isLoggedIn ? (
+            <>
+              <Link href="/profile">내 프로필</Link>
+              <Link href="/logout">로그아웃</Link>
+              <Link href="/notifications">
+                <Image src={NotificationIcon} alt="알림 아이콘"></Image>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/login">로그인</Link>
+              <Link href="/signup">회원가입</Link>
+            </>
+          )}
+        </div>
+      </div>
+
+      {!isTablet && (
+        <div className={cx("searchBar-container-mobile")}>
           <input
             className={cx("searchBar")}
             type="text"
@@ -37,25 +86,7 @@ export default function NavigationBar({}) {
             alt="검색 아이콘"
           />
         </div>
-
-        {/* Buttons */}
-        <div className={cx("buttons-container")}>
-          {isLoggedIn ? (
-            <>
-              <Link href="/profile">내 프로필</Link>
-              <Link href="/logout">로그아웃</Link>
-              <Link href="/notifications">
-                <Image src={NotificationIcon} alt={""}></Image>
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link href="/login">로그인</Link>
-              <Link href="/signup">회원가입</Link>
-            </>
-          )}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
