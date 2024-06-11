190d9c50160319378466d5af31a5e0e1ae18009e
BaeUiJin <baeuijin01@gmail.com>
2024-06-11 18:30:52 +0900

    remove: 불필요한 파일 제거

--- pages/NoticeEdit.tsx
+++
@@ -1,3 +0,0 @@
-export default function NoticeEdit() {
-  return <div>NoticeEdit</div>;
-}
--- src/components/common/ui-card-notice/CardNotice.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import classNames from 'classnames/bind';
-import styles from './CardNotice.module.scss';
-
-const cx = classNames.bind(styles);
-
-export const CardNotice = () => <div className={cx('container')}></div>;
