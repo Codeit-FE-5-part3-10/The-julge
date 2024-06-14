42c02d0f755a4d408b6cfa631dfd43170376ad9a
morrison <morrison.k@hpcnt.com>
2024-06-14 17:38:40 +0900

    axios instance 개선(쿼리 직렬화)

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
+import qs from 'qs';
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  paramsSerializer: (parameters) =>
+    qs.stringify(parameters, { arrayFormat: 'repeat', encode: false }),
 });
